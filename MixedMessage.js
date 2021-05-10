69a2ee51cda6493c1bfbbbe6786b8435e3c8f211
Piotr Pawlik <59952821+kilwap@users.noreply.github.com>
2021-05-10 19:44:44 +0200

    added generating random commentary for football match

--- MixedMessage.js
+++ MixedMessage.js
@@ -1,4 +1,41 @@
+// this project will generate random commentary from football action
+
+// function to help cheose random message from list
 const getRand = limit => {
     return Math.floor(Math.random() * limit);
 }
-console.log(getRand(5));
+
+//storing types of messages
+const messageData = {
+    player:['Drogba', 'Lampard', 'Hazard', 'Mata', 'Torres'],
+    actionType:['attacking on the right', 'going down from the right', 'dribling in the middle'],
+    actionResult:['what a goal!', 'hit the post', 'what a save from the keeper', 'oh no last second tackle stoped him from scoring']
+}
+
+// store messsage in array
+let commentary = [];
+
+//iterate over the object
+for(part in messageData){
+    let sentenceId = getRand(messageData[part].length)
+
+    switch (part) {
+        case 'player':
+            commentary.push(`Chelsea got the ball and ${messageData[part][sentenceId]} is`);
+            break;
+        case 'actionType':
+            commentary.push(`${messageData[part][sentenceId]} has great opportunities to shoot!`);
+            break;
+        case 'actionResult':
+            commentary.push(`${messageData[part][sentenceId]}, great attack from Chelsea`);
+            break;
+        default:
+            commentary.push('sorry for problems we will be back with broadcast soon')
+            break;
+    }
+}
+
+function formatMessage(commentary){
+    console.log(commentary.join(' '))
+};
+formatMessage(commentary);
